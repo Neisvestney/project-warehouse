89acfe168993629428cce1aed3adf77c7fa23925
neisvestney <neisvestney113@gmail.com>
2026-05-25 19:42:46 +0300

    Fix useDrawerSearchParamsState

--- projectwarehouse.client/src/hooks/useDrawerSearchParamsState.ts
+++ projectwarehouse.client/src/hooks/useDrawerSearchParamsState.ts
@@ -1,23 +1,31 @@
+import {useRef} from "react";
 import {useNavigate, useSearchParams} from "react-router";
 
 export function useDrawerSearchParamsState(name: string) {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const historyLengthOnOpen = useRef<number | null>(null);
 
   const selectedItemId = searchParams.get(name);
 
   const openDrawer = (id: string) => {
+    historyLengthOnOpen.current = window.history.length;
     const next = new URLSearchParams(location.search);
     next.set(name, id);
     navigate(`?${next.toString()}`);
   };
 
   const closeDrawer = () => {
-    console.log("close");
-    const next = new URLSearchParams(location.search);
     if (!searchParams.has(name)) return;
-    next.delete(name);
-    navigate(`?${next.toString()}`, {replace: true});
+    if (historyLengthOnOpen.current !== null) {
+      const delta = window.history.length - historyLengthOnOpen.current;
+      historyLengthOnOpen.current = null;
+      navigate(-delta);
+    } else {
+      const next = new URLSearchParams(location.search);
+      next.delete(name);
+      navigate(`?${next.toString()}`, {replace: true});
+    }
   };
 
   return [selectedItemId, openDrawer, closeDrawer] as const;
